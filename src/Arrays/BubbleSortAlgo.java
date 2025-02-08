74a1b2dead2fe1b8b0d4d928f551eba11afc3bdd
nandangitdesk <patilnandan859@gmail.com>
2025-02-09 00:31:23 +0530

    learn about n^2 sorting techniques

--- src/Arrays/BubbleSortAlgo.java
+++ src/Arrays/BubbleSortAlgo.java
@@ -21,11 +21,42 @@ public class BubbleSortAlgo {
         System.out.println(" ");
     }
 
+    //selectionSorting - O(n^2)
+    public static void selectionSort(int numbers[]){
+        for (int i = 0; i < numbers.length-1; i++) {
+            int maxPosition = i;
+            for (int j = i+1; j < numbers.length; j++) {
+                if (numbers[j] < numbers[maxPosition]){
+                    maxPosition = j;
+                }
+            }
+            int temp = numbers[maxPosition];
+            numbers[maxPosition] = numbers[i];
+            numbers[i] = temp;
+        }
+    }
+
+    //insertion sort
+    public  static void insertionSort(int numbers[]) {
+        for (int i = 1; i < numbers.length ; i++) {
+            int curr = numbers[i];
+            int prev = i-1;
+            while (prev >=0 && numbers[prev] > curr){
+                numbers[prev+1] = numbers[prev];
+                prev--;
+            }
+            //insertion -
+            numbers[prev+1] = curr;
+        }
+    }
+
     public static void main(String args[]) {
         int numbers[] = {5,6,2,1,4,3};
 
-        bubbleSort(numbers);
-
+//        bubbleSort(numbers);
+//          selectionSort(numbers);
+        insertionSort(numbers);
         printArray(numbers);
+
     }
 }
